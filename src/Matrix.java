2839717e5d168856d33ee0e5fbb9d4458eb34187
Schmibbler <shauncomino@gmail.com>
2022-06-06 10:41:52 -0400

    Initial commit

---
+++ src/Matrix.java
@@ -0,0 +1,13 @@
+public class Matrix {
+    private double [][] array;
+
+    public Matrix (int rows, int cols) {
+        this.array = new double[rows][cols];
+    }
+    public Matrix () {
+        this.array = null;
+    }
+
+
+
+}
